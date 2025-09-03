4eecbb67642fb75167af46ba04a8de717a5e3707
ljh <3538788638@qq.com>
2025-09-03 14:32:06 +0800

    feat(*) 线程池

---
+++ JucLearning/src/main/java/Day5/ThreadPoolDemo.java
@@ -0,0 +1,29 @@
+package Day5;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();//单个线程
+        ExecutorService threadPool2 = Executors.newFixedThreadPool(5); //创建一个固定的线程池的大小
+        ExecutorService threadPool3 = Executors.newCachedThreadPool(); //可伸缩的
+
+        //线程池用完必须要关闭线程池
+        try {
+
+            for (int i = 1; i <=100 ; i++) {
+                //通过线程池创建线程
+                threadPool3.execute(()->{
+                    System.out.println(Thread.currentThread().getName()+ " ok");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+}
